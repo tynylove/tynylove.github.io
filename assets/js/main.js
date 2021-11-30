bb44226435c3919076619b21d9e77620305e7145
tynylove <47032110+tynylove@users.noreply.github.com>
2021-11-30 10:17:10 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,7 +112,7 @@
 
 	// Poptrox.
 		$main.poptrox({
-			onPopupOpen: function() { $body.addClass('is-poptrox-visible'); },
+			onPopupOpen: function() { $body.addClass('is-poptrox-visible');startmusic();},
 			onPopupClose: function() { $body.removeClass('is-poptrox-visible'); },
 			overlayColor: '#1a1f2c',
 			overlayOpacity: 0.75,
@@ -388,33 +388,47 @@
 			})();
 			
 	// Set the date we're counting down to
-				var countDownDate = new Date("Dec 19, 2021 00:00:00").getTime();
+	var countDownDate = new Date("Dec 19, 2021 00:00:00").getTime();
 
-				// Update the count down every 1 second
-				var x = setInterval(function() {
+	// Update the count down every 1 second
+	var x = setInterval(function() {
 
-				  // Get today's date and time
-				  var now = new Date().getTime();
-					
-				  // Find the distance between now and the count down date
-				  var distance = countDownDate - now;
-					
-				  // Time calculations for days, hours, minutes and seconds
-				  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-				  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-				  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-				  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-					
-				  // Output the result in an element with id="countdown"
-				  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-				  + minutes + "m " + seconds + "s till our wedding";
-					
-				  // If the count down is over, write some text 
-				  if (distance < 0) {
-					clearInterval(x);
-					document.getElementById("countdown").innerHTML = "Save the day 19-12-2021";
-				  }
-				}, 1000);	
+	  // Get today's date and time
+	  var now = new Date().getTime();
+		
+	  // Find the distance between now and the count down date
+	  var distance = countDownDate - now;
+		
+	  // Time calculations for days, hours, minutes and seconds
+	  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		
+	  // Output the result in an element with id="countdown"
+	  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+	  + minutes + "m " + seconds + "s till our wedding";
+		
+	  // If the count down is over, write some text 
+	  if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("countdown").innerHTML = "Save the day 19-12-2021";
+	  }
+	}, 1000);	
 	
+	var startmusic = (function() {
+		var executed = false;
+		return function() {
+			if (!executed) {
+				executed = true;
+				var widget1 = SC.Widget("backgroundmusic");
+					widget1.play();
+				var menuPaused = document.getElementById("menu-paused");
+					menu.style.display = "block";
+			}
+		};
+	})();
+	
+	document.addEventListener('click', () => {startmusic()}, { once: true });			
 
 })(jQuery);
